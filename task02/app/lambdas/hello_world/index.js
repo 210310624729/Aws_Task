c586f1f5902668ff237e7afdade62b5405fad00f
ashim_banerjee <ashim_banerjee@epam.com>
2025-03-10 15:15:26 +0530

    task02

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -1,18 +1,35 @@
-exports.handler = async (event) => {
-    const { path, httpMethod } = event;
-
-    if (path === "/hello" && httpMethod === "GET") {
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ message: "Hello from Lambda" }),
+const routes = {
+    "/hello": {
+      GET: () => ({
+        "statusCode": 200,
+        "body": {
+          "statusCode": 200,
+          "message": "Hello from Lambda",
+        },
+        "headers": {
+          "content-type": "application/json",
+        },
+        "isBase64Encoded": false,
+      }),
+    },
+  };
+  
+  exports.handler = async (event) => {
+    const path = event.rawPath || "/";
+    const method = event.requestContext?.http?.method || "UNKNOWN";
+    const routeHandler = routes[path]?.[method];
+  
+    return routeHandler
+      ? routeHandler()
+      : {
+          "statusCode": 400,
+          "body": {
+            "statusCode": 400,
+            "message": `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`,
+          },
+          "headers": {
+            "content-type": "application/json",
+          },
+          "isBase64Encoded": false,
         };
-    }
-
-    
-    return {
-        statusCode: 400,
-        body: JSON.stringify({
-            error: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${httpMethod}`,
-        }),
-    };
-};
+  };
