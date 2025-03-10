3c81b231163aa13539dada9f309863be8cb68d28
ashim_banerjee <ashim_banerjee@epam.com>
2025-03-10 14:53:11 +0530

    task02

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -3,13 +3,13 @@ exports.handler = async (event) => {
     if (path === "/hello" && httpMethod === "GET") {
       return {
         statusCode: 200,
-        body: JSON.stringify({ message: "Hello, World!" }),
+        body: JSON.stringify({ message: "Hello from Lambda" }),
       };
     }
     return {
       statusCode: 400,
       body: JSON.stringify({
-        error: `Bad Request: {path} {method} is not allowed`,
+        error: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${httpMethod}`,
       }),
     };
   };
